cda21de3ecb599485e560f06f86a8083abe5ff7f
Enmanuel <emachipana0102@gmail.com>
2022-04-08 17:21:40 -0500

    ADD: Method Start

--- restaunrant-insights.rb
+++ restaunrant-insights.rb
@@ -6,13 +6,54 @@ class RestInsights
     end
 
     def start
+        puts "\n"
         welcome
+        menu
+        puts "\n"
+        print "> "
+        option, action = gets.chomp.split
+        until option == "exit"
+            case option
+            when "menu" then menu
+            when "1" then puts "list of restaurants here"
+            when "2" then puts "list of dishes here"
+            when "3" then puts "distribution users here"
+            when "4" then puts "top ten by visitors here"
+            when "5" then puts "top ten by sales here"
+            when "6" then puts "top ten by avg per user here"
+            when "7" then puts "avg consumer expenses here"
+            when "8" then puts "total sales by month here"
+            when "9" then puts "best price for dish here"
+            when "10" then puts "favorite dish here"
+            else puts "Enter a valid option please"
+            end
+            print "> "
+            option, action = gets.chomp.split
+        end
     end
 
     def welcome
-        message = ["Welcome to the Restaurants Insights!",
-        "Write 'menu' at any moment to print the menu again and 'quit' to exit."]
-        puts message
+        puts [
+            "Welcome to the Restaurants Insights!",
+            "Write 'menu' at any moment to print the menu again and 'quit' to exit."
+        ]
+    end
+
+    def menu
+        puts "-" * 5
+        puts [
+            " 1. List of restaurants included in the research filter by ['' | category=string | city=string]",
+            " 2. List of unique dishes included in the research",
+            " 3. Number and distribution (%) of clients by [group=[age | gender | occupation | nationality]]",
+            " 4. Top 10 restaurants by the number of visitors.",
+            " 5. Top 10 restaurants by the sum of sales.",
+            " 6. Top 10 restaurants by the average expense of their clients.",
+            " 7. The average consumer expense group by [group=[age | gender | occupation | nationality]]",
+            " 8. The total sales of all the restaurants group by month [order=[asc | desc]]",
+            " 9. The list of dishes and the restaurant where you can find it at a lower price.",
+            " 10. The favorite dish for [age=number | gender=string | occupation=string | nationality=string]",
+            ]
+        puts "-" * 5 
     end
 end
 
